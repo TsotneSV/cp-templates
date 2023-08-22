ef9256adcb675409484546bd204db62d0fc1935c
TsotneSV <112477768+TsotneSV@users.noreply.github.com>
2023-08-22 14:25:51 +0400

    Create inc.cpp

---
+++ inc.cpp
@@ -0,0 +1,48 @@
+#pragma gcc diagnostic "-std=c++1z"
+#include <bits/stdc++.h>
+using namespace std;
+/* /\_/\
+  (= ._.)
+  / >  \>
+*/
+//#define int long long
+
+#define fi first
+#define se second
+#define pb push_back
+#define ins insert
+#define mp make_pair
+#define endl "\n"
+#define sz(x) (int) (x).size()
+#define deb(x) cout<<(x)<<endl
+#define all(x) (x).begin(),(x).end()
+#define dbg(x) cerr<<#x<<" "<<x<<endl
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vpii> vpii;
+typedef vector<vpll> vpll;
+
+//const ll MOD=998244353;
+const ll MOD=1e9+7;
+const ll INF=1e9;
+const int MAXN=2e5+5;
+
+
+
+void solve(){
+
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+    int tt=1;
+    cin>>tt;
+    while(tt--){
+        solve();
+    }
+}
