c148163b033bc1a9cf860cf4e6333a2a5a65fd31
TsotneSV <112477768+TsotneSV@users.noreply.github.com>
2023-08-22 14:15:47 +0400

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,29 @@
+struct DSU {
+
+    vector<int> parent,SIZE;
+
+    DSU(int n) {
+        parent.resize(n + 1);
+        SIZE.resize(n + 1);
+        for(int i=1;i<=n;i++) {
+            parent[i] = i;
+            SIZE[i] = 1;
+        }
+    }
+
+    int find_set(int u) {
+        if(u == parent[u]) return u;
+        return parent[u] = find_set(parent[u]);
+    }
+
+    void union_sets(int u,int v) {
+        u = find_set(u);
+        v = find_set(v);
+
+        if(u != v) {
+            if(SIZE[u] < SIZE[v]) swap(u,v);
+            parent[v] = u;
+        }
+    }
+
+};
