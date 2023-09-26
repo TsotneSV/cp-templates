e2ce48b574709edcf3974c06d83ba5db06039dd8
TsotneSV <112477768+TsotneSV@users.noreply.github.com>
2023-09-26 23:06:33 +0400

    Update and rename inc.cpp to tempalte.cpp

--- tempalte.cpp
+++ tempalte.cpp
@@ -1,4 +1,7 @@
 #pragma gcc diagnostic "-std=c++1z"
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
 #include <bits/stdc++.h>
 using namespace std;
 /* /\_/\
@@ -7,29 +10,27 @@ using namespace std;
 */
 //#define int long long
 
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
 #define fi first
 #define se second
 #define pb push_back
 #define ins insert
 #define mp make_pair
-#define endl "\n"
+#define endl '\n'
 #define sz(x) (int) (x).size()
-#define deb(x) cout<<(x)<<endl
 #define all(x) (x).begin(),(x).end()
+#define deb(x) cout<<(x)<<endl
 #define dbg(x) cerr<<#x<<" "<<x<<endl
 
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
-
-//const ll MOD=998244353;
-const ll MOD=1e9+7;
-const ll INF=1e9;
-const int MAXN=2e5+5;
+const int inf=1e9,MAXN=2e5+5;
+const ll INF=1e18;
 
 
 
@@ -38,8 +39,7 @@ void solve(){
 }
 
 signed main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);cin.tie(0);
     int tt=1;
     cin>>tt;
     while(tt--){
