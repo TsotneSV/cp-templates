b97c4120956d7b4ce76b58ed0fa6f12db6e36a0b
TsotneSV <112477768+TsotneSV@users.noreply.github.com>
2023-10-07 14:41:15 +0400

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,47 +1,60 @@
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-
 #include <bits/stdc++.h>
 using namespace std;
 /* /\_/\
   (= ._.)
   / >  \>
 */
-//#define int long long
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
-using ll = long long;
-using pii = pair<int,int>;
-using pll = pair<ll,ll>;
-using vi = vector<int>;
-using vll = vector<ll>;
-using vpii = vector<pii>;
-using vpll = vector<pll>;
-
+// #define int long long
 #define fi first
 #define se second
 #define pb push_back
 #define ins insert
 #define mp make_pair
+#define send {ios_base::sync_with_stdio(false);}
+#define help {cin.tie(0);}
 #define endl '\n'
-#define sz(x) (int) (x).size()
+#define sz(x) ((long long) (x).size())
 #define all(x) (x).begin(),(x).end()
 #define deb(x) cout<<(x)<<endl
 #define dbg(x) cerr<<#x<<" "<<x<<endl
 
-const int inf=1e9,MAXN=2e5+5;
-const ll INF=1e18;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+void fileIO(string filename) {
+	freopen((filename + ".in").c_str(), "r", stdin);
+	freopen((filename + ".out").c_str(), "w", stdout);
+}
+
+// const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+
+const int inf=1e9,MAXN=2e5+5; 
+const ll INF=1e18; 
+const ld pi = 3.14159265358979323846;
 
 
 
-void solve(){
+void solve(int tc = 0){
 
 }
 
 signed main(){
-    ios_base::sync_with_stdio(false);cin.tie(0);
-    int tt=1;
-    cin>>tt;
-    while(tt--){
-        solve();
-    }
+  
+    send help
+  
+    int tc=1;
+    // cin>>tc;
+    for(int t = 0; t < tc; t++) solve(t);
 }
