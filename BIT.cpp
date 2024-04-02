26ffbd294410de1c0535e35e16837427b91cd952
TsotneSV <112477768+TsotneSV@users.noreply.github.com>
2024-04-02 19:07:46 +0400

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,23 +1,23 @@
 struct BIT {
-    int n; vi S;
+    int n; vector<long long> S;
 
     void update(int pos,int add) {
         for(pos;pos<=n;pos+=pos&-pos) S[pos]+=add;
     }
 
-    int sum(int pos) {
-        int ret=0;
+    ll sum(int pos) {
+        ll ret=0;
         for(pos;pos>0;pos-=pos&-pos) ret += S[pos];
         return ret;
     }
 
-    int query(int l,int r) {
+    ll query(int l,int r) {
         return sum(r) - sum(l-1);
     }
 
-    BIT(vi v) {
+    BIT(vector<int> v) {
         n = v.size();
-        S = vi(n+1,0);
+        S = vector<long long>(n+1,0);
         for(int i=0;i<n;i++) update(i+1,v[i]);
     }
 };
