e1456cc333f8364827992d219e77a793b808a472
TsotneSV <112477768+TsotneSV@users.noreply.github.com>
2024-08-09 20:13:02 +0400

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,30 +1,23 @@
-struct HashedString {
-    const ll mod = 1e9 + 7;
-    const ll p = 125213;
- 
-    int n;
-    vector<ll> pw;
-    vector<ll> h;
- 
-    void build(string s) {
-        n = (int)s.size();
-        pw.resize(n + 1);
-        h.resize(n + 1);
-        pw[0] = 1;
-        h[0] = 0;
-        for (int i = 0; i < n; i += 1) {
-            pw[i + 1] = (pw[i] * p) % mod;
-            h[i + 1] = (h[i] * p + (s[i] - 'a' + 1)) % mod;
-        }
+struct BIT {
+    int n; vector<long long> S;
+
+    void update(int pos,int add) {
+        for(pos;pos<=n;pos+=pos&-pos) S[pos]+=add;
     }
- 
- 
-    ll get(int l, int r) {
-        return ((h[r + 1] - h[l] * pw[r - l + 1]) % mod + mod) % mod;
+
+    ll sum(int pos) {
+        ll ret=0;
+        for(pos;pos>0;pos-=pos&-pos) ret += S[pos];
+        return ret;
     }
- 
- 
-    ll rget(int l, int r) {
-        return get(n - r - 1, n - l - 1);
+
+    ll query(int l,int r) {
+        return sum(r) - sum(l-1);
+    }
+
+    BIT(vector<int> v) {
+        n = v.size();
+        S = vector<long long>(n+1,0);
+        for(int i=0;i<n;i++) update(i+1,v[i]);
     }
 };
